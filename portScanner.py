e8a1eab4473e72cb1e7aada5c8c9bd926a3789e2
Michael Cosmadelis <MCosmadelis@users.noreply.github.com>
2017-01-10 21:38:24 -0500

    added OS detection, banner grabbing, args

--- portScanner.py
+++ portScanner.py
@@ -1,40 +1,51 @@
-#!/usr/bin/python
-
 """
-A simple TCP port scanner.
+TCP Port Scanner with general OS detection and
+banner grabbing.
 By: Michael Cosmadelis
 """
-
 import socket
 import sys
+import subprocess
 
 def main():
-    # prompts the user for input
-    host = raw_input("Enter host IP to scan: ")
-    hostip = socket.gethostbyname(host)
-    # checks the validity of the IP inputted
-    if len(host.split(".")) != 4:
-        print("Target host must be a valid IPv4 address")
-        return 1
-    print "=" * 25
-    print("Scanning " + host + " please wait...")
+	try:
+		ip = sys.argv[1]
+		lport = int(sys.argv[2])
+		uport = int(sys.argv[3])
+		pingResult = subprocess.check_output("ping -c 1 %s" % ip, shell=True)
 
-    try:
-        # performs a TCP connection on each port
-        for port in range(1, 1000):
-            sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sckt.settimeout(2)
-            result = sckt.connect_ex((hostip, port))
-            # if connection is successful that port is open
-            if result == 0:
-                print("Port {} is open.").format(port)
-            sckt.close()
-    except KeyboardInterrupt:
-        print("Scan stopped.")
-        sys.exit()
-    except socket.error:
-        print("Couldn't connect to host")
-        sys.exit()
+		if "ttl=128" in pingResult:
+			print "Target is running Windows."
+		elif "ttl=64" in pingResult:
+			print "Target is running Linux."
+		elif "ttl=255" in pingResult:
+			print "Target is running BSD."
+
+
+		for port in range(lport, uport):
+			try:
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				s.settimeout(1) #socket times out after 1 second
+
+				result = s.connect((ip, port))
+					if result == 0:
+						print "Port %i is open on %s" % (port ,ip)
+				banner = s.recv(4096)
+				if not banner =="":
+					print"\t%s" % banner
+				s.close()
+
+				except socket.error:
+				print "Port %i is closed on %s" % (port, ip)
+
+				except socket.timeout:
+				print "Port %i is filtered on %s" % (port, ip) 
+				
+				except KeyboardInterrupt:
+					print "Scan stopped."
+						sys.exit()
+		except (NameError, IndexError) as e:
+			print "Usage: python banner.py [ip] [start port] [end port]"
 
 if __name__ == '__main__':
-    sys.exit(main())
+	sys.exit(main())
